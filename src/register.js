088a0e9a2b7a9b0ea8b87e0370445c084b41ba0e
Qunli <qunli02@gmail.com>
2020-06-11 23:26:38 -0400

    register button

--- src/register.js
+++ src/register.js
@@ -7,7 +7,22 @@ class Register extends React.Component {
   render(){
       return(
         <div>
-          
+        <form>
+        <h1>
+        Register
+        </h1>
+          <label>
+            ID:
+            <input type="text" name="name" />
+          </label>
+          <br/>
+          <label>
+            Password:
+            <input type="password" name="password" />
+          </label>
+          <br/>
+          <input type="submit" value="Register" />
+        </form>
         </div>
       )
     }
